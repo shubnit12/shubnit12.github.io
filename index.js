c4845ee92124898ae380d63b5ae756049841ea7c
shubnit12 <shubnit12@gmail.com>
2025-06-18 17:59:05 +0530

    Fix resume route case sensitivity and update download link in pp.html

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "pp.html"));
 });
 
-app.get("/Resume", (req, res) => {
+app.get("/resume", (req, res) => {
   console.log("Downloading Resume");
   async function downloadFile(realFileId) {
     async function authenticate() {
